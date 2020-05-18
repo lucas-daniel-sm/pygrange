c710bed0e3ce86620f0d756fc0a35d24d92bdb21
Lucas Daniel <61884218+lucas-daniel-sm@users.noreply.github.com>
2020-05-18 18:15:38 -0300

    logica para calcular o erro quase concluida

--- src/lagrange_error.py
+++ src/lagrange_error.py
@@ -1,5 +1,6 @@
 from lagrange_solver import LagrangeSolver
-from sympy import Symbol
+from sympy import Symbol, diff, Function
+from cartesian_point import CartesianPoint
 
 
 class LagrangeError:
@@ -8,30 +9,67 @@ class LagrangeError:
     point: float
     order: int
 
-    def __init__(self, original_function, lagrange_polinomial, points: list, point: float):
-        self.original_function = original_function 
-        self.lagrange_polinomial = lagrange_polinomial
-        self.points = points
-        self.point = point
+    def __init__(self, config: map):
+        self.lagrange_polinomial = config['lagrange_polinomial']
+        #self.original_function = config['original_function ']
+        self.function = config['function']
+        self.points = config['points']
+        self.point = config['point']
+        self.order = len(self.points)
 
     def calculate(self):
-        multiplication = multiplication_point_points()
-        m = calculate_m()
-        expression = (f'{multiplication} * ({m}/({order}!)')
-        print('Erro = {expression}')
-        result = expression.subs(Symbol('x'), point)
-        print(result)
+        m = self.calculate_m()
+        multiplication = self.multiplication()
+        expression = (f'{multiplication} * ({m}/({self.order}!))')
+        factorial_order_plus_one = 6 #self.order!
+        result = multiplication * m / factorial_order_plus_one
+        print(f'E_t({self.point}) = {expression} = {result}')
 
     def calculate_m(self):
-        for i in range(len(points)):
-            print(f'derivada {1} = ')
+        last_derivative = self.function
+        derivatives = []
+        last_value: float
+        maxValue = None
+        values = []
+        x = Symbol('x')
 
-    def multiplication_point_points(self):
-        expression = ''
-        for local_point in points:
-            expression = (f'{expression} * ({point} - {local_point})')
-        simplified_expression = LagrangeSolver.simplify_expression(expression)
-        print(f'expressao: {expression}')
-        print (f'simplificado: {simplified_expression}')
+        for i in range(self.order):
+            last_derivative = diff(last_derivative, x)
+            derivatives.append(last_derivative)
+        
+        for point in self.points:
+            value = last_derivative.subs(x, point.x)
+            values.append(value)
+            if maxValue is None:
+                maxValue = value
+            else:
+                if value > maxValue:
+                    maxValue = value
+    
+        print(f'calaculo do M = {maxValue}')
+        print(f'\t >> derivadas:')
+        for derivative in derivatives:
+            print(f'\t\t{LagrangeSolver.simplify_expression(derivative, x)}')
+        print(f'\t >> valores para a {self.order + 1}ª derivada:')
+        for value in values:
+            print(f'\t\t{value}')
+        return maxValue
 
-        return 
+    def multiplication(self):
+        expression = None
+        acumulator = None
+        for local_point in self.points:
+            if expression is None:
+                expression = (f'({self.point} - {local_point.x})')
+            else:
+                expression = (f'{expression} * ({self.point} - {local_point.x})')
+            if acumulator is None:
+                acumulator = self.point - local_point.x
+            else:
+                acumulator = acumulator * (self.point - local_point.x)
+
+        print('Produtório')
+        print(f'\t >> expressao: {expression}')
+        print(f'\t >> resultado: {acumulator}\n')
+        
+        return acumulator
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@
 import json
 from cartesian_point import CartesianPoint
 from lagrange_solver import LagrangeSolver
+from lagrange_error import LagrangeError
 
 y_0 = 1.1414
 y_1 = 1.2649
@@ -19,5 +20,17 @@ points = [
 lagrange_solver = LagrangeSolver(points)
 result = lagrange_solver.solve()
 result_json = lagrange_solver.resultToJson(result)
-print(result_json)
 
+def calcular_erro():
+    print(result_json)
+    print('\n')
+    lagrange_error = LagrangeError({
+        'lagrange_polinomial': result['final_polynomial'],
+        'function': 'sqrt(1+x)',
+        'points': points,
+        'point': 0.45
+    })
+    lagrange_error.calculate()
+
+# Caso não queira calcular o erro descomente a linha abaixo
+calcular_erro()
